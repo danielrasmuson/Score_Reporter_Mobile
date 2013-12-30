957e9b506aac6ea1f35c5d7eac7b192987df6f19
danielrasmuson <dan123911@gmail.com>
2013-12-29 20:32:02 -0600

    Added Map Extraction - Other Adjustments (see below)
    Made sections attributes of the class per your recommendation Nate.
    
    Also added testing via files.
    
    Other minor adjustments

--- Deploy/Extraction/run.py
+++ Deploy/Extraction/run.py
@@ -10,43 +10,56 @@ def getSource(url):
         page_source = browser.page_source
     return page_source
 
+def toFile(fileName,text):
+    textFile = open("tests\\all\\"+fileName + ".html","w")
+    cssSheets = '<link rel="stylesheet" href="..\\allCSS.css"><link rel="stylesheet" href="..\\allCSS2.css">'
+    textFile.write(cssSheets + text)
+    textFile.close()
+    
 
 class tournamentPageInfo():
     """docstring for tournamentPageInfo"""
     def __init__(self, sourceCode):
         self.soup = BeautifulSoup(sourceCode)
-    def getPageName(self):
-        return self.soup.title.string
-    def getDescription(self):
-        return self.soup.find('table', { "class" : "tourninfo" })
-    def getPools(self):
-        """Returns a list of all pools in page"""
+        self.pageName = self.soup.title.string
+        self.description = self.soup.find('table', { "class" : "tourninfo" })
+        self.pools, self.brackets = self.findPoolsAndBrackets()
+        self.map = self.soup.find('div',{'class':'gm-style'})
+    def findPoolsAndBrackets(self):
+        """The reason only 1 function is that they are in the same element"""
         pools = self.soup.findAll('table', { "class" : "pool" })
-        resultList = []
+        poolList = []
+        bracketList = []
         for pool in pools:
             poolName = pool.find('td', { "class" : "poolname" }).get_text()
             if str(poolName).split()[0] == "Pool":
-                resultList.append(str(pool))
-
-        return resultList
+                poolList.append(str(pool))
+            else:
+                bracketList.append(str(pool))
+        return [poolList,bracketList]
+    def getPageName(self):
+        return str(self.pageName)
+    def getDescription(self):
+        return str(self.description)
+    def getPools(self):
+        return self.pools
     def getBrackets(self):
-        """Returns a list of all brackets in page"""
-        #@TODO only difference is "!=" and "=="
-        pools = self.soup.findAll('table', { "class" : "pool" })
-        resultList = []
-        for pool in pools:
-            poolName = pool.find('td', { "class" : "poolname" }).get_text()
-            if str(poolName).split()[0] != "Pool":
-                resultList.append(str(pool))
+        return self.brackets
+    def getMap(self):
+        """Currently just returns the html of the 
+        initial picture I.E. is not interactive"""
+        return str(self.map)
 
-        return resultList
+# # Source from site
+sourceCode = getSource("http://scores.usaultimate.org/scores/#masters/tournament/12982")
 
-# sourceCode = getSource("http://scores.usaultimate.org/scores2012/#masters/tournament/11648")
-
-textFile = open("bracketTest\\sourceCode.txt","r")
-sourceCode = textFile.read()
-textFile.close()
+# # Source from file
+# textFile = open("tests\\sourceCode.html","r")
+# sourceCode = textFile.read()
+# textFile.close()
 
 tournamentPage = tournamentPageInfo(sourceCode)
-print(tournamentPage.getDescription())
-# print("<br><br><br>".join(tournamentPage.getBrackets()))
+toFile(tournamentPage.pageName + " description",tournamentPage.getDescription())
+toFile(tournamentPage.pageName + " brackets","<br><br><br>".join(tournamentPage.getBrackets()))
+toFile(tournamentPage.pageName + " pools","<br><br><br>".join(tournamentPage.getPools()))
+toFile(tournamentPage.pageName + " map",tournamentPage.getMap())
